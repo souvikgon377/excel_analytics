3ca8a64a52cd6a3c9127f7caf9105a7f83dd178c
Souvik Gon <140043113+souvikgon377@users.noreply.github.com>
2025-07-15 01:09:53 +0530

    Delete client directory

--- client/src/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  // For development - points to your local server
-  API_BASE_URL: 'https://excel-analytics-war5.onrender.com',
-  
-  // For production - uncomment this line when deploying
-  // API_BASE_URL: 'https://excel-analytics-war5.onrender.com',
-};
-
-export default config;
